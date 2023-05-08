7c1d0fa93f90b49dbb0381aec16520e6558c1871
Michael Wender <michael@michaelwender.com>
2023-05-08 10:48:13 -0400

    First commit

---
+++ sellers-json.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Sellers.json Editor
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Implements the sellers.json standard for your WordPress site.
+ * Author:          TheWebist
+ * Author URI:      https://mwender.com
+ * Text Domain:     sellers-json
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sellers_Json
+ */
+
+// Your code starts here.
